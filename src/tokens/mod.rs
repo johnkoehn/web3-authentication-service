cf5270001c79e5ec0cb8af46953685449ede6f5b
John Koehn <koehn.john95@gmail.com>
2022-01-22 21:50:10 -0600

    Endpoint working :)

--- src/tokens/mod.rs
+++ src/tokens/mod.rs
@@ -2,6 +2,7 @@ use serde::{Serialize, Deserialize};
 use jsonwebtoken::{encode, decode, Header, Algorithm, Validation, EncodingKey, DecodingKey};
 use actix_web::{post, web, HttpResponse, Responder};
 use std::time::{SystemTime, Duration, UNIX_EPOCH};
+use std::error::Error;
 
 
 // Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
@@ -19,6 +20,13 @@ pub struct CreateTokenRequest {
     public_key: String
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TokenResponse {
+    access_token: String,
+    token_type: String,
+    expires_in: u64
+}
+
 #[post("/tokens")]
 pub async fn create_token(create_token_web_request: web::Json<CreateTokenRequest>) -> impl Responder {
     let create_token_request = create_token_web_request.into_inner();
@@ -29,20 +37,49 @@ pub async fn create_token(create_token_web_request: web::Json<CreateTokenRequest
 
     let iat = match now.duration_since(UNIX_EPOCH) {
         Ok(n) => n.as_secs(),
-        Err(_) => panic!("Failed to calculate iat"),
+        Err(err) => {
+            println!("Failed to calculate iat: {}", err);
+            return HttpResponse::InternalServerError().finish()
+        }
     };
 
     let exp = match expires_at.duration_since(UNIX_EPOCH) {
         Ok(n) => n.as_secs(),
-        Err(_) => panic!("Failed to calculate exp"),
+        Err(err) => {
+            println!("Failed to calculate exp: {}", err);
+            return HttpResponse::InternalServerError().finish()
+        }
     };
 
-
-    let claim = Claims {
+    let claims = Claims {
         sub: create_token_request.public_key,
         iat,
         exp
     };
 
+    let key = b"secret";
+
+    let mut header = Header::default();
+    header.kid = Some("signing_key".to_owned());
+    header.alg = Algorithm::HS256;
+
+    let token = match encode(&header, &claims, &EncodingKey::from_secret(key)) {
+        Ok(t) => t,
+        Err(err) => {
+            println!("Error creating token: {}", err);
+            return HttpResponse::InternalServerError().body("")
+        }
+    };
+
+    let response = TokenResponse {
+        access_token: token,
+        token_type: "Bearer".to_string(),
+        expires_in: twenty_four_hours.as_secs()
+    };
+
+    let response_as_json = serde_json::to_string(&response).unwrap();
+
     HttpResponse::Ok()
+        .content_type("application/json")
+        .body(&response_as_json)
 }
